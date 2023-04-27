d5ae475d8ab7bb0752e0cd223010ef9cffd82e12
abdulmahl <mahalanguma87@gmail.com>
2023-04-27 20:54:26 +0200

    Files moved to folder.

--- scripts/app.js
+++
@@ -1,4 +0,0 @@
-let d = new Date();
-let fullDate = `${d.toDateString()} | ${d.toLocaleTimeString()}`;
-document.querySelector("#dateTime").textContent = fullDate;
-document.querySelector("#year").textContent = d.getFullYear();
