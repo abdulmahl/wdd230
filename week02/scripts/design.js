d4dda7fad99183966b7a6f97829270634a1ae40a
abdulmahl <mahalanguma87@gmail.com>
2023-04-28 19:29:41 +0200

    .

--- week02/scripts/design.js
+++ week02/scripts/design.js
@@ -5,6 +5,7 @@ document.querySelector("#dateTime").textContent = fullDate;
 document.querySelector("#year").textContent = date.getFullYear();
 
 document.querySelector("#myName").style.fontSize = "large";
+document.querySelector("#year").style.fontSize = "large";
+
 document.querySelector("#update").style.fontSize = "small"; 
 document.querySelector("#dateTime").style.fontSize = "small"; 
-document.querySelector("#year").style.fontSize = "large";
