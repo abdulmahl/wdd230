b0de7899bf09183605954a4953ea843b36704a3a
abdulmahl <mahalanguma87@gmail.com>
2023-04-28 19:00:04 +0200

    .

--- week02/scripts/design.js
+++ week02/scripts/design.js
@@ -1,7 +1,5 @@
 let date = new Date();
 
-let fullDate = `${date.toDateString()} ${date.toLocaleTimeString()}`;
+let fullDate = `${date.toDateString()} @ ${date.toLocaleTimeString()}`;
 document.querySelector("#dateTime").textContent = fullDate;
 document.querySelector("#year").textContent = date.getFullYear();
-const line = document.querySelector(".contact");
-line.style.position = "center";
