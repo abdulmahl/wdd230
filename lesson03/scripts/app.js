537f78d45376ab51fe66d6c224708aee7011e81d
abdulmahl <mahalanguma87@gmail.com>
2023-05-03 21:16:34 +0200

    .

--- lesson03/scripts/app.js
+++ lesson03/scripts/app.js
@@ -9,7 +9,7 @@ let fullDateandTime = `${date.toLocaleString("en-US", {
 document.querySelector("#year").textContent = date.getFullYear();
 document.querySelector("#dateTime").textContent = fullDateandTime;
 
-function Person(firstName, lastName, age, email, purpose) {
+let Person = function(firstName, lastName, age, email, purpose) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.fullName = `${firstName}  ${lastName}`;
@@ -20,13 +20,15 @@ function Person(firstName, lastName, age, email, purpose) {
 
 let person1 = new Person("Elizabeth", "Mfati", 57, "remfati@edu.co.za", "advertising online");
 let persona1 = document.querySelector(".persona1");
+// let persona1Name = document.querySelector(".persona1-name");
 
-persona1.textContent = `1. ${person1.fullName} a ${person1.age} year old woman who wants to grow her 
+
+persona1.textContent = `is a ${person1.age} year old woman who wants to grow her 
 business by ${person1.purpose}. Her email address is ${person1.email}. Mrs ${person1.lastName} is a primary school teacher who owns a small pre-school.`;
 
 let person2 = new Person("Emmanuel", "Masinga", 46, "imza@millitarybase.co.za", "grow his network");
 let persona2 = document.querySelector(".persona2");
 
-persona2.textContent = `2. ${person2.fullName}, ${person2.age} year old male is looking to ${person2.purpose}.
+persona2.textContent = `2.is a ${person2.age} year old male is looking to ${person2.purpose}.
 His email address is ${person2.email}. Mr ${person2.lastName} is SANDF employee who also in his off days 
 makes deliveries for Uber-Eats, Mr D Foods, and other well known restuarants.`
