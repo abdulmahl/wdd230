3debaad8def1c781c4d0a3fa6105db57c2b09bea
abdulmahl <mahalanguma87@gmail.com>
2023-05-03 21:18:21 +0200

    .

--- lesson03/scripts/app.js
+++ lesson03/scripts/app.js
@@ -20,8 +20,6 @@ let Person = function(firstName, lastName, age, email, purpose) {
 
 let person1 = new Person("Elizabeth", "Mfati", 57, "remfati@edu.co.za", "advertising online");
 let persona1 = document.querySelector(".persona1");
-// let persona1Name = document.querySelector(".persona1-name");
-
 
 persona1.textContent = `is a ${person1.age} year old woman who wants to grow her 
 business by ${person1.purpose}. Her email address is ${person1.email}. Mrs ${person1.lastName} is a primary school teacher who owns a small pre-school.`;
@@ -29,6 +27,6 @@ business by ${person1.purpose}. Her email address is ${person1.email}. Mrs ${per
 let person2 = new Person("Emmanuel", "Masinga", 46, "imza@millitarybase.co.za", "grow his network");
 let persona2 = document.querySelector(".persona2");
 
-persona2.textContent = `2.is a ${person2.age} year old male is looking to ${person2.purpose}.
+persona2.textContent = `is a ${person2.age} year old male is looking to ${person2.purpose}.
 His email address is ${person2.email}. Mr ${person2.lastName} is SANDF employee who also in his off days 
 makes deliveries for Uber-Eats, Mr D Foods, and other well known restuarants.`
