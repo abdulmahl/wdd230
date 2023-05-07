cd27073fa58e4f429f4b9091698759dd5599fe45
abdulmahl <mahalanguma87@gmail.com>
2023-05-07 20:38:41 +0200

    Deleted files

--- css-specificity-presentation/scripts/app.js
+++
@@ -1,9 +0,0 @@
-let date = new Date();
-let fullDateAndTime = `${date.toLocaleString("en-US", {
-    dateStyle: "medium",
-    timeStyle: "short",
-    hour12: false,
-})}`;
-
-document.querySelector("#year").textContent = date.getFullYear();
-document.querySelector("#dateTime").textContent = fullDateAndTime;
