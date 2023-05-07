606b00ffe9ad85cd644f152125978a0784ec10ff
abdulmahl <mahalanguma87@gmail.com>
2023-05-07 20:34:54 +0200

    Folder moved

--- start-navigation/js/scripts.js
+++
@@ -1,7 +0,0 @@
-let toggleMenu = function() {
-    document.querySelector("#primaryNav").classList.toggle("open");
-    document.querySelector("#hamburgerBtn").classList.toggle("open");
-}
-
-const x = document.querySelector("#hamburgerBtn");
-x.onclick = toggleMenu;
