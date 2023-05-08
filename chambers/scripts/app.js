912061ee76af6bfe1b55a9d46d188be6b002447f
abdulmahl <mahalanguma87@gmail.com>
2023-05-08 23:12:24 +0200

    .

--- chambers/scripts/app.js
+++ chambers/scripts/app.js
@@ -12,3 +12,11 @@ let date = new Date(Date.UTC(2023, 05, 08,));
 let dateString = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(date);
 
 document.querySelector(".dateAndTime").textContent = dateString;
+
+let toggleMenu = function() {
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.querySelector("#hamburgerBtn");
+x.onclick = toggleMenu;
