ccec41e40063b072cff2c6af91e3a1e72beae959
abdulmahl <mahalanguma87@gmail.com>
2023-05-08 23:10:55 +0200

    .

--- chambers/scripts/app.js
+++ chambers/scripts/app.js
@@ -1,3 +1,13 @@
+let d = new Date();
+let fullDateandTime = `${d.toLocaleString("en-US", {
+    dateStyle: "medium",
+    timeStyle: "short",
+    hour12: false,
+})}`;
+
+document.querySelector("#dateTime").textContent = fullDateandTime;
+document.querySelector("#year").textContent = d.getFullYear();
+
 let date = new Date(Date.UTC(2023, 05, 08,));
 let dateString = new Intl.DateTimeFormat('en-GB', { dateStyle: 'full' }).format(date);
 
