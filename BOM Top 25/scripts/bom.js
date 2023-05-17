e89fb0942e6ea5e337cf333fb630090597a9b5b0
abdulmahl <mahalanguma87@gmail.com>
2023-05-17 12:09:52 +0200

    .

--- BOM Top 25/scripts/bom.js
+++ BOM Top 25/scripts/bom.js
@@ -1,3 +1,28 @@
 const date = new Date();
 
 document.querySelector("#year").textContent = date.getFullYear();
+
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const buttton = document.querySelector("button");
+
+buttton.addEventListener("click", () => {
+    const myBook = input.value;
+    input.value = "";
+    
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
+
+    listItem.append(listText);
+    listText.textContent = myBook;
+    listItem.append(listBtn);
+    listBtn.textContent = "❌";
+    list.append(listItem);
+
+    listBtn.addEventListener("click", () => {
+        list.removeChild(listItem);
+    });
+
+    input.focus();
+});
