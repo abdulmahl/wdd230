38e61ced74f2627d4bfd3d4865f0d6a17d1150d6
abdulmahl <mahalanguma87@gmail.com>
2023-05-25 12:23:39 +0200

    .

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,12 +1,12 @@
-const temp = document.querySelector(".description-base");
+const temp = document.querySelector(".description");
 temp.textContent = "33°C";
 
 const windSpeed = 25;
-const wind = document.querySelector(".wind-base");
+const wind = document.querySelector(".wind");
 wind.textContent = `Wind Speed: ${windSpeed} km/h`;
 
 const windChill = "N/A"
-const chill = document.querySelector(".wind-chill-base");
+const chill = document.querySelector(".wind-chill");
 chill.textContent = `Wind Chill: ${windChill}`;
 
 
