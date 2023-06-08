7a1f20524466a990548fa25474dc12d771a02c4f
abdulmahl <mahalanguma87@gmail.com>
2023-06-08 12:03:05 +0200

    .

--- chamber/scripts/validatetitle.js
+++ chamber/scripts/validatetitle.js
@@ -2,7 +2,7 @@ const positionTitle = document.querySelector(".position-title");
 const titleError = document.querySelector(".valid-title");
 
 positionTitle.addEventListener("input", function(e) {
-    const pattern = /[A-Za-z\s-]{7,}$/;
+    const pattern = /^[A-Za-z\s-]{7,}$/;
     const currentValue = e.target.value;
     const valid = pattern.test(currentValue);
 
