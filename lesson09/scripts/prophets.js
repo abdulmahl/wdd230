e13a460d33937d44d40d54fbd963501eda300bfb
abdulmahl <mahalanguma87@gmail.com>
2023-06-08 13:44:04 +0200

    .

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,3 +1,14 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = response.json();
+    console.table(data.prophets);
+
+}
+
+getProphetData();
+
 let date = new Date();
 let fullDateandTime = `${date.toLocaleString("en-ZA", {
     dateStyle: "medium",
