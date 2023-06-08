28d102d787de1b9692272b6a5e04a99afe786880
abdulmahl <mahalanguma87@gmail.com>
2023-06-08 14:07:11 +0200

    .

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -3,11 +3,33 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 async function getProphetData() {
     const response = await fetch(url);
     const data = response.json();
-    console.table(data.prophets);
-
+    displayProphets(data.prophets);
 }
 
 getProphetData();
+
+const displayProphets = (prophets) => {
+    const cards = document.querySelector("div.cards");
+    
+    prophets.forEach((prophet) => {
+        let card = document.createElement("section");
+        let h2 = document.createElement("h2");
+        let portrait = document.createElement("img");
+
+        h2.textContent = `${prophet.name} ____________`;
+
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Portrait of ${prophet.name} ____________`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "340");
+        portrait.setAttribute("heigth", "440");
+
+        card.appendChild(h2);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
 
 let date = new Date();
 let fullDateandTime = `${date.toLocaleString("en-ZA", {
