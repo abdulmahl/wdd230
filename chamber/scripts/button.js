4358fddc381636491d41c886a0c2e5c4ac9b25db
abdulmahl <mahalanguma87@gmail.com>
2023-06-13 21:24:27 +0200

    .

---
+++ chamber/scripts/button.js
@@ -0,0 +1,20 @@
+// Create variables to hold the grid and the 
+// list buttons.
+const gridButton = document.querySelector('#grid');
+const listButton = document.querySelector('#list');
+const display = document.querySelector('article');
+
+// Create a functions to display either a list or a grid.
+let showList = function() {
+    display.classList.add('list');
+    display.classList.remove('grid');
+};
+
+listButton.addEventListener('click', showList());
+
+let showGrid = function() {
+    display.classList.add('grid');
+    display.classList.remove('list');
+};
+
+gridButton.addEventListener('click', showGrid());
--- chamber/scripts/json.js
+++ chamber/scripts/json.js
@@ -11,17 +11,25 @@ fetch(url)
     // Put the json formatted data into a variable called companies.
     .then( (jsonObject) => {
         const companies = jsonObject['companies'];
-        // Select the html element to house the json data.
-        const cards = document.querySelector('div.cards');
+        // Select the html element to house the json data
+        // and display the data as grid.
+        const cards = document.querySelector('article.cards');
 
-        // Create elements  and then append them into the main section selected above "json data".
+        // Select another html element to display the data as a list.
+        const businessList = document.querySelector('div.business-list');
+
+        // Dynamicly add elements  and then append them into the main section
+        // selected above "json data".
         companies.forEach((company) => {
+            let list = document.createElement('table');
+
             let card = document.createElement('section');
             let logo = document.createElement('img');
             let membership = document.createElement('p');
             let phoneNumber = document.createElement('p');
             let address = document.createElement('p');
             let anchor = document.createElement('a');
+            let name = document.createElement('td');
 
             logo.setAttribute('src', company.iconurl);
             logo.setAttribute('alt', `Logo of ${company.name}`);
@@ -42,5 +50,23 @@ fetch(url)
             card.append(anchor);
 
             cards.append(card);
+
+            address = document.createElement('td');
+            phoneNumber = document.createElement('td');
+            anchor = document.createElement('td');
+
+            anchor.setAttribute('href', company.companyurl)
+
+            name.textContent = company.name;
+            address.textContent = company.address;
+            phoneNumber.textContent = company.phoneNumber;
+            anchor.textContent = company.anchor;
+
+            list.append(name);
+            list.append(address);
+            list.append(phoneNumber);
+            list.append(anchor);
+
+            businessList.append(list);
         });  
     });
