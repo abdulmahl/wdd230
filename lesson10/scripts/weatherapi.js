06a7bd6b348fac500284b1c88fdf4c1fcaf0bc92
abdulmahl <mahalanguma87@gmail.com>
2023-06-19 13:01:29 +0200

    Initial commit

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,23 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captiobDesc = document.querySelector('figcaption');
+const apiKey = 'cb266327ed92e9d613d6ebe5e148d6ca';
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&d=fairbanks&units=imperail&appid=' + this.apiKey;
+
+async function fetchApi()  {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        } else {
+            throw Error(await response.text());
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fetchApi();
