cdcf8a8bbbe0a40ed50e613c830adff3f3acae92
abdulmahl <mahalanguma87@gmail.com>
2023-06-19 15:56:49 +0200

    .

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,16 +1,16 @@
-const currentTemp = document.querySelector('#cyrrent-temp');
+const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captiobDesc = document.querySelector('figcaption');
-// const apiKey = 'cb266327ed92e9d613d6ebe5e148d6ca';
+const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&d=Fairbanks&units=imperail&appid={cb266327ed92e9d613d6ebe5e148d6ca}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=64.84&lon=-147.72&d=Fairbanks&units=imperail&appid=cb266327ed92e9d613d6ebe5e148d6ca';
 
-async function fetchApi()  {
+let fetchApi = async function()  {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
+            // console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -21,3 +21,9 @@ async function fetchApi()  {
 }
 
 fetchApi();
+
+let displayResults = function(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed()}</strong>`;
+
+    
+};
