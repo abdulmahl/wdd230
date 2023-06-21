52898403b6a392aa1e38755580d6e16698bc28ae
abdulmahl <mahalanguma87@gmail.com>
2023-06-21 17:59:25 +0200

    .

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -2,9 +2,11 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
+const apiKey = 'cb266327ed92e9d613d6ebe5e148d6ca';
+
 const url = 'https://api.openweathermap.org/data/2.5/weather?'
     + 'lat=64.84&lon=-147.72&d=Fairbanks&units=imperail&appid='
-    + 'cb266327ed92e9d613d6ebe5e148d6ca';
+    + apiKey;
 
 let fetchApi = async function() {
     try {
