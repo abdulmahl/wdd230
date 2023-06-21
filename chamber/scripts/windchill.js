6f86b8cbc8cb23d27c7eb057e3e193508bd5ffdb
abdulmahl <mahalanguma87@gmail.com>
2023-06-21 13:20:33 +0200

    .

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -3,12 +3,17 @@
 const temp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const caption = document.querySelector('.description');
+const windSpeed = document.querySelector('.wind-speed');
+const windChill = document.querySelector('.wind-chill');
 
 // Select the html tags to target for the base 
 // viewport.
 const tempSmall = document.querySelector('#current-temp-small');
 const smallWeatherIcon = document.querySelector('#weather-icon-small');
 const smallCaption = document.querySelector('.description-small');
+const windSpeedSmall = document.querySelector('.wind-speed-small');
+const windChillSmall = document.querySelector('.wind-chill-small');
+
 
 // Put the API url in variable.
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-26.08&lon=27.98&d=Randburg&units=metric&appid=cb266327ed92e9d613d6ebe5e148d6ca';
@@ -35,6 +40,7 @@ fetchApi();
 // Create the function that will display the results.
 let displayResults = function(weatherData) {
 
+    // Small viewport display.
     const smallIconSrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const smallScription = weatherData.weather[0].description;
 
@@ -42,11 +48,11 @@ let displayResults = function(weatherData) {
     smallWeatherIcon.setAttribute('alt', smallScription);
 
     tempSmall.innerHTML = `<strong>${weatherData.main.temp.toFixed()}&deg;C</strong> <br>`;
-
     smallCaption.textContent = `${smallScription.toUpperCase()}`;
+    windSpeedSmall.textContent = `Wind Speed: ${weatherData.wind.speed} km/h`;
+    windChillSmall.textContent = `Wind Chill: N/A`;
 
-    temp.innerHTML = `Temperature: <strong>${weatherData.main.temp.toFixed()}&deg;C</strong>`;
-
+    // Medium viewport display.
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const scription = weatherData.weather[0].description;
 
@@ -55,5 +61,9 @@ let displayResults = function(weatherData) {
     weatherIcon.setAttribute('width', '90');
     weatherIcon.setAttribute('height', '90');
 
+    temp.innerHTML = `<strong>${weatherData.main.temp.toFixed()}&deg;C</strong>`;
     caption.textContent = scription.toUpperCase();
+    windSpeed.textContent = `Wind Speed: ${weatherData.wind.speed} km/h`;
+    windChill.textContent = `Wind Chill: N/A`;
+
 };
