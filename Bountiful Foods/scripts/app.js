c9a40f0a5a61535cbd73f59c70b7fa8a74dd5e73
abdulmahl <mahalanguma87@gmail.com>
2023-07-02 17:14:40 +0200

    .

--- Bountiful Foods/scripts/app.js
+++ Bountiful Foods/scripts/app.js
@@ -32,7 +32,7 @@ lrgForecast.textContent = 'Randburg Five Day Weather Forecast'
 const apiKey = 'cb266327ed92e9d613d6ebe5e148d6ca';
 
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?'
-    + 'lat=-26.08&lon=27.98&d=Randburg&units=metric&appid=' 
+    + 'lat=-26.08&lon=27.98&d=Carlsbad&units=metric&appid=' 
     + apiKey;
     
 fetch(apiUrl)
