d6543a974809bc488a85adde7a54bd9fea2648f0
abdulmahl <mahalanguma87@gmail.com>
2023-07-04 17:18:57 +0200

    .

--- Bountiful Foods/scripts/weather.js
+++ Bountiful Foods/scripts/weather.js
@@ -1,5 +1,5 @@
 const lrgForecast = document.querySelector('.forecast');
-lrgForecast.textContent = 'Carlsbad Five Day Weather Forecast'
+lrgForecast.textContent = 'Carlsbad Five Day Weather Forecast';
 
 const apiKey = 'cb266327ed92e9d613d6ebe5e148d6ca';
 
@@ -11,22 +11,22 @@ fetch(apiUrl)
     .then(response => response.json())
     .then(jsonData => {
 
-        for(let i = 0; i < 5; i ++) {
+        for(let i = 0; i < 4; i ++) {
             document.querySelector('#day' + (i + 1)).textContent = weekDay[checkDay(i)];
         }
 
-        for(let i = 0; i < 5; i ++) {
+        for(let i = 0; i < 4; i ++) {
             document.querySelector('#img' + (i + 1)).src = `https://openweathermap.org/img/w/${jsonData.list[0].weather[0].icon}.png`;
         }
 
-        for(let i = 0; i < 5; i ++) {
+        for(let i = 0; i < 4; i ++) {
             document.querySelector('#temp' + (i + 1)).textContent = jsonData.list[0].main.temp.toFixed(0);
         }
 
-        for(let i = 0; i < 5; i ++) {
+        for(let i = 0; i < 4; i ++) {
             document.querySelector('#cond' + (i + 1)).textContent = jsonData.list[0].weather[0].description;
         }
-    })
+    });
 
 let weekDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -37,24 +37,3 @@ function checkDay(day) {
         return day + date.getDay();
     }
 }
-    
-
-            
-        
-            
-        
-            // const temp = document.createElement('p');
-            // temp.classList.add('temperature');
-            // temp.innerHTML = `<strong>${weatherData.list[0].main.temp.toFixed(0)}&deg;C</strong>`;
-        
-            // const condition = document.createElement('p');
-            // condition.classList.add('card-description');
-            // condition.textContent = description;
-        
-            // card.append(temp);
-            // card.append(condition);
-        
-
-
-
-
