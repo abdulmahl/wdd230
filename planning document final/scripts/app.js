75c82937589d617421db642034d660c3816b9240
abdulmahl <mahalanguma87@gmail.com>
2023-07-05 11:25:59 +0200

    Deleted files

---
+++ planning document final/scripts/app.js
@@ -0,0 +1,4 @@
+let date = new Date();
+let fullDate = `${date.toDateString()} @ ${date.toLocaleTimeString()}`;
+document.querySelector("#dateTime").textContent = fullDate;
+document.querySelector("#year").textContent = date.getFullYear();
