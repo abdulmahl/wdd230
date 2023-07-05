449b174f72070aaf5ecfd464446f383ed744d2ce
abdulmahl <mahalanguma87@gmail.com>
2023-07-05 14:34:50 +0200

    .

--- planning document final/scripts/app.js
+++ planning document final/scripts/app.js
@@ -1,4 +1,4 @@
 let date = new Date();
-let fullDate = `${date.toDateString()} @ ${date.toLocaleTimeString()}`;
+let fullDate = `${date.toDateString()}`;
 document.querySelector("#dateTime").textContent = fullDate;
 document.querySelector("#year").textContent = date.getFullYear();
